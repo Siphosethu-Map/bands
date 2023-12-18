2587f858edcc3cf6a7193f42805d3111074fc058
Siphosethu <mmapekula.sm17@gmail.com>
2023-12-18 22:45:22 +0200

    second models.py commit

--- bands/models.py
+++ bands/models.py
@@ -3,7 +3,13 @@ from django.db import models
 
 """ Create your models here.
  Band class, where the parameters of the data the user will input is created
- this model is found in the administration side """
+ this model is found in the administration side
+
+ this class will create a:
+ name field: band name,
+ genre field: band genre
+ formed_date: when the band was formed
+ description: band description"""
 
 
 class Band(models.Model):
