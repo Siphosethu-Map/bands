7ac7597004bc401eb33a9aa0ded1fbdea5f90ce5
Siphosethu <mmapekula.sm17@gmail.com>
2023-12-18 22:02:59 +0200

    forms.py commit

--- bands/forms.py
+++ bands/forms.py
@@ -3,6 +3,10 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
+""" Created a form for registration form for the users first
+name with max length of 35"""
+
+
 # we create a class for the first name
 class RegisterForm(UserCreationForm):
     first_name = forms.CharField(max_length=35, required=True,
