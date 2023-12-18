21beeb6ba815dacbe9294817185b06cb267aa7e7
Siphosethu <mmapekula.sm17@gmail.com>
2023-12-18 21:56:23 +0200

    models.py commit

--- bands/models.py
+++ bands/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 
-# Create your models here.
-# Band class, where the parameters of the data the user will input is created
-# this model is found in the administration side
+""" Create your models here.
+ Band class, where the parameters of the data the user will input is created
+ this model is found in the administration side """
+
 
 class Band(models.Model):
     name = models.CharField(max_length=80, null=False, blank=False)
