7e8c0cba99860a6607dc6d2409ff8a4fa114786c
Siphosethu <mmapekula.sm17@gmail.com>
2023-12-18 22:42:17 +0200

    second forms.py commit

--- bands/forms.py
+++ bands/forms.py
@@ -3,8 +3,12 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
-""" Created a form for registration form for the users first
-name with max length of 35"""
+""" Created a form for registration form for the users
+the form will ask the user for their
+first name
+username
+password1
+password2(comfirmation of password1)"""
 
 
 # we create a class for the first name
